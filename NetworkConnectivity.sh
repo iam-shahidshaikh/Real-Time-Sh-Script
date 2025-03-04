f3ba5e24e081000d1a6f045999e6dca0adc3f840
shahid13in <shahidshaikh13in@gmail.com>
2025-03-05 00:49:02 +0530

    first commit

---
+++ NetworkConnectivity.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+HOST="google.com"
+OUTPUT_FILE="/home/ubuntu/output.txt"
+if ping -c 1 $HOST &> /dev/null
+then
+        echo "$HOST is reachable" >> $OUTPUT_FILE
+else
+        echo "$HOST is not reachable" >> $OUTPUT_FILE
+fi
